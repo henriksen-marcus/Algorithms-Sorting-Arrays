6bd288d5a36ca63f7829c491d5e01b7d7b87b6ad
Marcus Henriksen <89453098+henriksen-marcus@users.noreply.github.com>
2022-08-24 12:02:52 +0200

    Added bubblesort algorithm

--- Compulsory1_22H/Compulsory1_22H.cpp
+++ Compulsory1_22H/Compulsory1_22H.cpp
@@ -52,8 +52,9 @@ public:
 
     /* Enable this class to use square bracket operator like normal arrays.
     Effectively makes this an array class. */
-    T operator [](int i) const { return arr[i]; }
-    T operator =(int i, T newVal) const { arr[i] = newVal; }
+    T operator [](int i) { return arr[i]; }
+    //T operator =(T operator[](int i)) {arr[i] = i };
+    //T operator =(int i, T newVal) const { arr[i] = newVal; }
 };
 
 class test
@@ -62,21 +63,74 @@ public:
     int abc;
 };
 
+
+bool validate(vector<int> arr)
+{
+    for (int i{}; i < arr.size()-1; i++)
+    {
+        if (arr[i] > arr[i+1])
+        {
+            cout << "Validate: Array is not sorted" << endl;
+            return false;
+        }
+    }
+    cout << "Validate: Array is sorted" << endl;
+    for (int k{}; k < arr.size(); k++)
+    {
+        cout << arr[k] << " ";
+    }
+    cout << endl;
+    return true;
+}
+
+void swap(vector<int>* arr,int a, int b)
+{
+    int temp = arr->at(a);
+    arr->at(a) = arr->at(b);
+    arr->at(b) = temp;
+
+    cout << "swapped " << arr->at(b) << " with " << arr->at(a) << endl;
+}
+
+
+void bubblesort(vector<int> arr)
+{
+    while (!validate(arr))
+    {
+        for (int i{}; i < arr.size() - 1; i++)
+        {
+            if (arr[i] > arr[i+1])
+            {
+                swap(&arr, i, i + 1);
+
+                for (int k{}; k < arr.size(); k++)
+                {
+                    cout << arr[k] << " ";
+                }
+                cout << endl;
+                break;
+            }
+        }
+    }
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    //std::cout << "Hello World!\n";
 
-    vector1<int> obj(1);
+    //vector1<int> obj(1);
 
-    obj= [0, 500];
+    
 
-    cout << obj[0] << endl;
+    //obj[0] = 2;
 
-    test tst;
-    tst.abc = 1;
+    //cout << obj[0] << endl;
+    vector<int>myArr = { 5,4,3,2,1 };
+
+    bubblesort(myArr);
+
 
     return 0;
 }
-
 
 
